62cb9a4802c2b2bd414d55670fa01aefe8a3d709
riya <riyasindhu730@gmail.com>
2025-11-01 19:36:26 +0530

    create advanced_program file

--- advanced_program.js
+++ advanced_program.js
@@ -1,101 +1,25 @@
-// Program-1) Find the largest number in an array: Write a function that takes an array
-// of numbers and returns the largest number
-
-//Solution:=>
-{
-    let arr = [23, 67, 34, 99, 467, 267, 290]
-
-    let max = arr[0]
-    for (let i = 0; i < arr.length; i++) {
-        if (max < arr[i]) {
-            max = arr[i]
-        }
-    }
-    console.log("1) This solution is simple for loop =>", max);
-}
-
-{
-    let arr = [23, 67, 34, 99, 467, 267, 290]
-
-    let max = arr[0]
-    for (let i in arr) {
-        if (max < arr[i]) {
-            max = arr[i]
-        }
-    }
-    console.log("2) This solution is simple in loop =>", max);
-}
-
-{
-    let arr = [23, 67, 34, 99, 467, 267, 290]
-
-    let max = arr[0]
-    for (let i of arr) {
-        if (max < i) {
-            max = i
-        }
-    }
-    console.log("3) This solution is simple of loop =>", max);
-}
-
-{
-    let arr = [23, 67, 34, 99, 467, 267, 290]
-
-    let max = arr[0]
-    let i = 0
-    while (i < arr.length) {
-        if (max < arr[i]) {
-            max = arr[i]
-        }
-        i++
-    }
-    console.log("4) This solution is Simple While loop =>", max);
-}
 console.log();
-
-// Program-2) Write a function to calculate the sum of all element in an Array.
-
-//Solution:=>
+// Program-3) Reverse an array: Write a function to reverse the elements of 
+// an array without using built-in reverse() method.
 {
-    let arr = [34, 23, 5, 6, 9]
+    let arr = [2, 7, 8, 4, 3, 0]
 
-    let sum = 0
-    for (let i = 0; i < arr.length; i++) {
-        sum = sum + arr[i]
+    let reverse_function = []
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reverse_function += arr[i] + " "
     }
-    console.log("1) This Solution is simple for loop =>", sum);
+    console.log("1) This solution is Simple for loop=>", reverse_function);
 }
 
 {
-    let arr = [34, 23, 5, 6, 9]
+    let arr = [2, 7, 8, 4, 3, 0]
 
-    let sum = 0
-    for (let i in arr) {
-        sum = sum + arr[i]
+    let reverse_function = []
+    let i = arr.length - 1
+    while (i >= 0) {
+        reverse_function += arr[i] + " "
+        i--
     }
-    console.log("2) This Solution is simple in loop =>", sum);
-}
-
-{
-    let arr = [34, 23, 5, 6, 9]
-
-    let sum = 0
-    for (let i of arr) {
-        sum = sum + i
-    }
-    console.log("3) This Solution is simple of loop =>", sum);
-}
-
-{
-    let arr = [34, 23, 5, 6, 9]
-
-    let sum = 0
-
-    let i = 0
-    while (i < arr.length) {
-        sum = sum + arr[i]
-        i++
-    }
-    console.log("4) This Solution is simple While loop =>", sum);
+    console.log("2) This Solution is Simple while loop=>", reverse_function);
 }
 
