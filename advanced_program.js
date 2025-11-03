2cdb4fbb7193ce7b34c4af35409e4efbd5ae500a
riya <riyasindhu730@gmail.com>
2025-11-03 18:57:23 +0530

    add advanced_program file

--- advanced_program.js
+++ advanced_program.js
@@ -1,25 +1,213 @@
-console.log();
-// Program-3) Reverse an array: Write a function to reverse the elements of 
-// an array without using built-in reverse() method.
+//Program-1) Find the largest Number in an array: Write a function that takes an array
+//of numbers and returns the largest number
 {
-    let arr = [2, 7, 8, 4, 3, 0]
+    let arr = [23, 67, 34, 99, 467, 267, 290]
 
-    let reverse_function = []
-    for (let i = arr.length - 1; i >= 0; i--) {
-        reverse_function += arr[i] + " "
+    let max = arr[0]
+    for (let i = 0; i < arr.length; i++) {
+        if (max < arr[i]) {
+            max = arr[i]
+        }
     }
-    console.log("1) This solution is Simple for loop=>", reverse_function);
+    console.log("1) This solution is simple for loop =>", max);
 }
 
 {
-    let arr = [2, 7, 8, 4, 3, 0]
+    let arr = [23, 67, 34, 99, 467, 267, 290]
 
-    let reverse_function = []
+    let max = arr[0]
+    for (let i in arr) {
+        if (max < arr[i]) {
+            max = arr[i]
+        }
+    }
+    console.log("2) This solution is simple in loop =>", max);
+}
+
+{
+    let arr = [23, 67, 34, 99, 467, 267, 290]
+
+    let max = arr[0]
+    for (let i of arr) {
+        if (max < i) {
+            max = i
+        }
+    }
+    console.log("3) This solution is simple of loop =>", max);
+}
+
+{
+    let arr = [23, 67, 34, 99, 467, 267, 290]
+
+    let max = arr[0]
+    let i = 0
+    while (i < arr.length) {
+        if (max < arr[i]) {
+            max = arr[i]
+        }
+        i++
+    }
+    console.log("4) This solution is Simple While loop =>", max);
+}
+console.log();
+
+// Program-2) Write a function to calculate the sum of all element in an Array.
+
+//Solution:=>
+{
+    let arr = [34, 23, 5, 6, 9]
+
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i]
+    }
+    console.log("1) This Solution is simple for loop =>", sum);
+}
+
+{
+    let arr = [34, 23, 5, 6, 9]
+
+    let sum = 0
+    for (let i in arr) {
+        sum = sum + arr[i]
+    }
+    console.log("2) This Solution is simple in loop =>", sum);
+}
+
+{
+    let arr = [34, 23, 5, 6, 9]
+
+    let sum = 0
+    for (let i of arr) {
+        sum = sum + i
+    }
+    console.log("3) This Solution is simple of loop =>", sum);
+}
+
+{
+    let arr = [34, 23, 5, 6, 9]
+
+    let sum = 0
+    let i = 0
+    while (i < arr.length) {
+        sum = sum + arr[i]
+        i++
+    }
+    console.log("4) This Solution is simple while loop =>", sum);
+}
+
+console.log();
+
+// Program-3) Reverse an array: Write a function to reverse the elements of an array without
+//using built-in reverse() method
+{
+    let arr = [34, 7, 8, 4, 5, 3]
+
+    let reverse_arr = []
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reverse_arr += arr[i] + " "
+    }
+    console.log("1) This Solution is simple for loop=>", reverse_arr);
+}
+
+
+{
+    let arr = [34, 7, 8, 4, 5, 3]
+
+    let reverse_arr = []
     let i = arr.length - 1
     while (i >= 0) {
-        reverse_function += arr[i] + " "
+        reverse_arr += arr[i] + " "
         i--
     }
-    console.log("2) This Solution is Simple while loop=>", reverse_function);
+    console.log("2) This Solution is simple while loop =>", reverse_arr);
 }
+
+console.log();
+
+// Find the intersection of two arrays:
+// Write a function that takes two arrays and return a new array containg elements common
+// to both arrays.
+
+{
+    let arr1 = [3, 6, 3, 6, 79, 7]
+
+    let arr2 = [3, 5, 9, 79, 8, 7]
+    let intersection = []
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] == arr2[i]) {
+            intersection.push(arr1[i])
+        }
+    }
+    console.log("1) This solution is simple for loop =>", intersection);
+}
+
+{
+    let arr1 = [3, 6, 3, 6, 79, 7]
+
+    let arr2 = [3, 5, 9, 79, 8, 7]
+    let intersection = []
+    for (let i in arr1) {
+        if (arr1[i] == arr2[i]) {
+            intersection.push(arr1[i])
+        }
+    }
+    console.log("2) This Solution is simple in loop =>", intersection);
+}
+
+{
+    let arr1 = [3, 6, 3, 6, 79, 7]
+
+    let arr2 = [3, 5, 9, 79, 8, 7]
+    let intersection = []
+    let i = 0
+    while (i < arr1.length) {
+        if (arr1[i] == arr2[i]) {
+            intersection.push(arr1[i])
+        }
+        i++
+    }
+    console.log("3) This Solution is simple while loop =>", intersection);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
